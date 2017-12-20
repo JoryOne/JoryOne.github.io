7302857772d0148c0fee2dc5b9e45cd19a91babf
JoryOne <1442933378@qq.com>
2017-12-21 01:47:28 +0800

    add new music

--- list.js
+++ list.js
@@ -1,36 +1,19 @@
-var list = {
-            "data":[
-                        {
-                                "title":"朱七-青春",
-                                "src":"http://static.jory.wang/music/folk/朱七-青春.mp3"
-                       },
-                        {
-                                "title":"赵雷-青春无处安放",
-                                "src":"http://static.jory.wang/music/folk/赵雷-青春无处安放.mp3"
-                       },
-                        {
-                                "title":"靳松-独自旅行",
-                                "src":"http://static.jory.wang/music/folk/靳松-独自旅行.mp3"
-                       },
-                        {
-                                "title":"靳松-老路小路",
-                                "src":"http://static.jory.wang/music/folk/靳松-老路小路.mp3"
-                       },
-                        {
-                                "title":"蒋山-怎么办",
-                                "src":"http://static.jory.wang/music/folk/蒋山-怎么办.mp3"
-                       },
-                        {
-                                "title":"蒋山-面朝大海春暖花开",
-                                "src":"http://static.jory.wang/music/folk/蒋山-面朝大海春暖花开.mp3"
-                       },
-                        {
-                                "title":"蒋山-我干了你随意",
-                                "src":"http://static.jory.wang/music/folk/蒋山-我干了你随意.mp3"
-                       },
-                        {
-                                "title":"蒋山-西南小镇",
-                                "src":"http://static.jory.wang/music/folk/蒋山-西南小镇.mp3"
-                       }
-            ]
-    };
+var musicListData = [
+        "/folk/朱七-青春.mp3",
+        "/folk/赵雷-青春无处安放.mp3",
+        "/folk/靳松-独自旅行.mp3",
+        "/folk/靳松-老路小路.mp3",
+        "/folk/蒋山-怎么办.mp3",
+        "/folk/蒋山-面朝大海春暖花开.mp3",
+        "/folk/蒋山-我干了你随意.mp3",
+        "/folk/蒋山-西南小镇.mp3",
+
+        "/folk/纣王老胡 - 涩.mp3",
+        "/folk/纣王老胡 - 眼泪.mp3",
+        "/folk/曹方 - 南部小城.mp3",
+        "/folk/拇指姑娘乐队 - 云的衣裳.mp3",
+        "/like/蔡健雅 - 停格.mp3",
+        "/like/徐大乐 - 一个人的朝圣.mp3",
+        "/like/GALA - 我绝对不能失去你.mp3",
+        "/like/GALA - 追梦赤子心.mp3"
+];
