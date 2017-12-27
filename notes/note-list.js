560b889fe06813d3c5020fd5dce24a92c2df3d05
JoryOne <1442933378@qq.com>
2017-12-28 00:56:52 +0800

    init my notes

---
+++ notes/note-list.js
@@ -0,0 +1,15 @@
+[{
+    "title":"关于作者",
+    "description":"个人介绍",
+    "createTime":"2017/04/17",
+    "tag":["关于"],
+    "file":"about",
+},
+{
+    "title":"关于博客",
+    "description":"本博客的介绍",
+    "createTime":"2017/04/17",
+    "tag":["关于","博客"],
+    "file":"blog",
+}
+];
